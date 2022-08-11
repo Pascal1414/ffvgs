c17d6b5b5b4be6ea4d76ddc8614a49345a839e3a
Pascal Rieder <pascal.rieder@outlook.com>
2022-08-11 17:04:44 +0200

    update contact

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,19 @@
 function navButtonClick() {
     var navItems = document.getElementsByClassName("nav-item");
-    var i;
 
-    for (i = 0; i < navItems.length; i++) {
+    for (var i = 0; i < navItems.length; i++) {
         if (navItems[i].style.display === "block") {
             navItems[i].style.display = "none";
         } else {
             navItems[i].style.display = "block";
+            console.log("test");
         }
     };
+    /* var navlist = document.getElementsByTagName("nav");
+     for (var i = 0; i < navlist.length; i++) {
+
+         navlist[i].removeChild(document.getElementsByClassName("nav-top"));
+     }*/
 }
 
 function hideNavItems() {
