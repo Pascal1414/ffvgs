d3d4f51f52aec9dd1a2d074a27e5c6539c8b4c3e
Pascal Rieder <pascal.rieder@outlook.com>
2022-08-11 09:47:56 +0200

    Update Nav structure

--- js/imagepreview.js
+++ js/imagepreview.js
@@ -19,23 +19,23 @@ function imageClick(image) {
     imageClone.onclick = function() {
         return false;
     }
-    all = document.getElementsByTagName("section");
+    all = document.getElementsByTagName("main");
     for (i = 0; i < all.length; i++) {
         all[i].style.display = "none";
     }
     var nav = document.getElementsByTagName("nav");
     for (i = 0; i < nav.length; i++) {
-
         nav[i].style.display = "none";
+    }
+    var footer = document.getElementsByTagName("footer");
+    for (i = 0; i < nav.length; i++) {
+        footer[i].style.display = "none";
     }
 
 
 
     document.body.style.backgroundColor = "black";
-    var main = document.getElementsByTagName("main");
-    for (i = 0; i < main.length; i++) {
-        main[i].style.paddingTop = 0;
-    }
+
 
     imageClone.style.display = "block";
 
@@ -47,8 +47,6 @@ function imageClick(image) {
         imageprev[i].style.display = "flex";
         imageprev[i].style.height = window.innerHeight - 40 + "px";
     }
-
-    scrollTo(document.body);
 
 
     imagelist = getImageParent(image).children;
@@ -83,6 +81,11 @@ function exit() {
         nav[i].style.display = "flex";
     }
 
+    var footer = document.getElementsByTagName("footer");
+    for (i = 0; i < nav.length; i++) {
+        footer[i].style.display = "block";
+    }
+
     var imageprev = document.getElementsByClassName("image-preview");
     for (i = 0; i < imageprev.length; i++) {
         imageprev[i].removeChild(imageClone);
@@ -93,33 +96,6 @@ function exit() {
     document.body.style.backgroundColor = "#f9f9f9";
 
 
-    var hash = window.location.href.substring(window.location.href.indexOf("#") + 1);
-
-    if (hash == "home") {
-        cathegoryClick("home");
-    } else if (hash == "programm") {
-        cathegoryClick("programm");
-
-    } else if (hash == "organe") {
-        cathegoryClick("organe");
-
-    } else if (hash == "jugend") {
-        cathegoryClick("jugend");
-
-    } else if (hash == "berichte") {
-        cathegoryClick("berichte");
-
-    } else if (hash == "galerie") {
-        cathegoryClick("galerie");
-
-    } else if (hash == "kontakt") {
-        cathegoryClick("kontakt");
-
-    } else if (hash == "boot") {
-        cathegoryClick("boot");
-    } else {
-        cathegoryClick("home");
-    }
 
     currentimage.parentElement.parentElement.scrollIntoView();
 }
