676f357d26058131fee6448de74be17dbbb8ef77
Pascal Rieder <pascal.rieder@outlook.com>
2023-01-06 12:21:32 +0100

    Update backend

--- ffvgs-backend/Program.cs
+++ ffvgs-backend/Program.cs
@@ -1,3 +1,7 @@
+using ffvgs_backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = "Server=tcp:ffvgsdatabase.database.windows.net,1433;Initial Catalog=ffvgs;Persist Security Info=False;User ID=pascalrieder;Password=ffvgs-database12-;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+builder.Services.AddDbContext<ffvgsContext>(options => options.UseSqlServer(connection));
+
+
+
+
 
 var app = builder.Build();
 
