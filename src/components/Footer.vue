148acc7e90730ce07acfd2a4983ef41fb9b82bba
Pascal <pascal.rieder@outlook.com>
2023-03-06 22:36:05 +0100

    Update Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,29 +21,52 @@
 
 <style scoped>
 .footer {
-    background-color: black;
+    background-color: var(--black);
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
+    flex-wrap: wrap;
 }
+
 .footer div {
     padding-top: 100px;
     padding-bottom: 100px;
     display: flex;
     flex-direction: column;
+    margin: 0 30px 0 30px;
 }
 
-.footer div h3{
-    margin: 0 0 6px 0 ;
+.footer div h3 {
+    margin: 0 0 6px 0;
 }
 
-.footer div a{
-    color: #C5C6C7;
+.footer div a {
+    color: var(--color-text);
     text-decoration: none;
     transition: 0.2s;
 }
+
 .footer div a:hover {
     color: blue;
 }
 
+@media (max-width: 700px) {
+    .footer {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        padding: 100px 10% 100px 10%;
+        grid-row-gap: 100px;
+    }
+
+    .footer div {
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+}
+@media (max-width: 320px) {
+    .footer {
+        display: flex;
+        flex-direction: column;
+    }
+}
 </style>
