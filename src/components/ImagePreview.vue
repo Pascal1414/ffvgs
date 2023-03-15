2bbda1ff01036f5e537f565533966a566f99f04b
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-03-15 09:41:50 +0100

    Create ImagePreview

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,130 @@
+<template>
+    <div ref="imagePreview" class="background">
+        <div class="image-preview">
+            <button class="image-preview__close" @click="closePreview">X</button>
+            <button class="image-preview__prev" @click="prev">❮</button>
+            <button class="image-preview__next" @click="next">❯</button>
+            <div class="image-preview__image" :style="{ backgroundImage: 'url(' + currentImage.src + ')' }">
+                <!-- <div :style="{ backgroundImage: currentImage.src }"></div>
+                <img :src="currentImage.src" :alt="currentImage.alt"> -->
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    name: 'ImagePreview',
+    props: {
+        images: {
+            type: Array as () => Array<{ src: string, alt: string }>,
+            required: true
+        }
+    },
+    data() {
+        return {
+            currentIndex: 0,
+            tempHeight: "10px"
+        };
+    },
+    computed: {
+        currentImage(): any {
+            return this.images[this.currentIndex];
+        },
+    },
+    created() {
+        document.addEventListener('keyup', (event) => {
+            if (event.key === 'Escape') {
+                this.closePreview();
+            }
+        });
+
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowLeft') {
+                this.prev();
+            } else if (event.key === 'ArrowRight') {
+                this.next();
+            }
+        });
+
+        document.addEventListener('click', (event) => {
+            if (event.target === this.$refs.imagePreview) {
+                this.closePreview();
+            }
+        });
+
+    },
+    methods: {
+        closePreview() {
+            this.$refs.imagePreview.classList.toggle('closed');
+        },
+        prev() {
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        },
+        next() {
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        }
+    }
+}
+</script>
+<style lang="css" scoped>
+.closed {
+    display: none;
+}
+
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 100;
+}
+
+.image-preview {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+    height: 70%;
+    overflow: hidden;
+    z-index: 200;
+    background-color: black;
+    padding: 55px;
+    border-radius: 11px;
+}
+
+.image-preview__image {
+    height: 100%;
+    width: 100%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.image-preview__close {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    width: 50px;
+    height: 50px;
+}
+
+.image-preview__prev {
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    width: 50px;
+    height: 50px;
+}
+
+.image-preview__next {
+    position: absolute;
+    top: 50%;
+    right: 5px;
+    width: 50px;
+    height: 50px;
+
+}
+</style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,29 +1,46 @@
 <template>
     <GalleryItem title="Absenden 2021">
-        
+
         <DoubleImage image1="/images/galery/Trüschenfischen/IMG_20221105_081130844small.jpg"
             image2="/images/galery/Trüschenfischen/IMG_20221105_081130844small.jpg" />
 
         <TripleImage image1="/images/galery/Helferausflug22/IMG_0868small.jpg"
             image2="/images/galery/Helferausflug22/IMG_0868small.jpg"
             image3="/images/galery/Helferausflug22/IMG_0868small.jpg" />
-            
+
     </GalleryItem>
 
-
+    <ImagePreview :images="images" />
 </template>
 <script lang="ts">
-import DoubleImage from '@/components/gallery/DoubleImage.vue';
-import TripleImage from '@/components/gallery/TripleImage.vue';
-import GalleryItem from '@/components/gallery/GalleryItem.vue';
+import DoubleImage from '../components/gallery/DoubleImage.vue';
+import TripleImage from '../components/gallery/TripleImage.vue';
+import GalleryItem from '../components/gallery/GalleryItem.vue';
+import ImagePreview from '../components/ImagePreview.vue';
 
 export default {
     name: 'GalleryView',
     components: {
         GalleryItem,
         DoubleImage,
-        TripleImage
+        TripleImage,
+        ImagePreview
+    },
+    data() {
+        return {
+            images: [
+                {
+                    src: '/images/galery/Trüschenfischen/IMG_20221105_081130844small.jpg',
+                    alt: 'Trüschenfischen'
+                },
+                {
+                    src: '/images/galery/Helferausflug22/IMG_0868small.jpg',
+                    alt: 'Helferausflug 2022'
+                }
+            ]
+        };
     }
+
 }
 </script>
 <style scoped></style>
