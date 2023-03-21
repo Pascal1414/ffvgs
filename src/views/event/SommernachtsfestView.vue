3d5d8ac06f676c59c378b88e3b0b48e0d7fdd15e
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-03-21 14:18:25 +0100

    Update SommernachtsfestView

--- src/views/event/SommernachtsfestView.vue
+++ src/views/event/SommernachtsfestView.vue
@@ -1,6 +1,21 @@
 <template>
     <div id="sommernachtsfest">
         <h1>Sommernachtsfest</h1>
+        <p>
+            Am Samstag öffnen wir unsere Fischküche, an deren Theke feine Zanderfilets im Bierteig gebacken werden. Sie sind
+            in
+            der Regel bis 22 Uhr erhältlich. Auch am Sonntag gibt es die zarten Fischfilets, so lange, bis wir ausverkauft
+            sind.
+        </p>
+        <p>
+            Für jene Gäste, die lieber Grilladen geniessen, gibt es Würste oder Steaks vom Grill. Auch Pommes Frites werden
+            frisch zubereitet. Dazu bietet unsere Theke im Festzelt vielerlei passende Getränke sowie Kaffee.<br>
+            Weil ein sommerliches Fest am See eine fröhliche Sache ist, sorgen wir auch für musikalische Unterhaltung. Am
+            Samstagabend spielt eine Band zum Tanz auf, am Sonntagvormittag ab 11 Uhr findet das Frühschoppenkonzert
+            satt.
+        </p>
+        Wir freuen uns auf zahlreiche Besucherinnen und Besucher, die mit uns feiern!
+
     </div>
 </template>
 <script lang="ts">
