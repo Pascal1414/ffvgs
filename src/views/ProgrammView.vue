706fd9ce220b32d240fc2d50271ff271b067c2f5
Pascal <pascal.rieder@outlook.com>
2023-04-02 13:00:11 +0200

    Update Programm

--- src/views/ProgrammView.vue
+++ src/views/ProgrammView.vue
@@ -1,17 +1,24 @@
 <template>
-    <h1>Programm</h1>
+    <h1 class="text-4xl font-bold">Programm</h1>
 
-    <label for="role">Programm nach Mitgliedschaft anzeigen:</label>
-    <select name="mitgliedschaft" id="role" @change="selectionChanged($event)">
-        <option value="none">Alles anzeigen</option>
-        <option value="alle">Alle Mitglieder</option>
-        <option value="junioren">Junioren</option>
-        <option value="jugendgruppe">Jugendgruppe</option>
-    </select>
+    <div class="form-control">
+        <label for="role" class="label">
+            <span class="label-text">Programm nach Mitgliedschaft anzeigen:</span>
+        </label>
+        <select class="select select-bordered w-full max-w-xs  mb-4" name="mitgliedschaft" id="role"
+            @change="selectionChanged($event)">
+            <option value="none" selected>Alles anzeigen</option>
+            <option value="alle">Alle Mitglieder</option>
+            <option value="junioren">Junioren</option>
+            <option value="jugendgruppe">Jugendgruppe</option>
+        </select>
+    </div>
+
+
     <table class="table w-full">
         <tbody>
             <tr v-for="programmItem in currentList">
-                <td> 
+                <td>
                     <svg fill="#ffffff" width="27px" height="27px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                         <path v-if="alreadyHappened(programmItem)"
                             d="M5 16.577l2.194-2.195 5.486 5.484L24.804 7.743 27 9.937l-14.32 14.32z" />
@@ -58,7 +65,7 @@ export default defineComponent({
                     return false;
             return true;
         },
-        formatDate(date: string): string {            
+        formatDate(date: string): string {
             return new Date(date).toLocaleDateString('ch-DE')
         },
         selectionChanged(event: Event) {
