fc915e717a77e09c601a1a9b4312917ce8f987bf
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-04-03 14:12:26 +0200

    Update GaleryView

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,6 +1,5 @@
 <template>
     <!-- The button to open modal -->
-    <label for="my-modal-5" class="btn">open modal</label>
 
     <!-- grid for images -->
 
@@ -11,16 +10,11 @@
             <div class="card-body">
 
                 <h1 class="text-2xl font-bold mb-2">{{ event.name }}</h1>
-                <div v-for="images in event.images">
-                    <div v-if="images.length === 2" class="flex flex-row justify-between mb-[1%] gap-3">
-                        <img class="w-[50%] rounded-box" :src="images[0].url" :alt="images[0].caption" />
-                        <img class="w-[50%] rounded-box" :src="images[1].url" :alt="images[1].caption" />
-                    </div>
-                    <div v-else class="flex flex-row justify-between mb-[1%] gap-3">
-                        <img class="w-[50%] rounded-box" :src="images[0].url" :alt="images[0].caption" />
-                        <img class="w-[50%] rounded-box" :src="images[1].url" :alt="images[1].caption" />
-                        <img class="w-[50%] rounded-box" :src="images[2].url" :alt="images[2].caption" />
-                    </div>
+                <div v-for="images in event.images" class="flex flex-row justify-between mb-[1%] gap-3">
+                    <label v-for="image in images" for="my-modal-5" class="w-[50%]"
+                        @click="() => { previewImage = image.url }">
+                        <img class="rounded-box" :src="image.url" :alt="image.caption" />
+                    </label>
                 </div>
             </div>
         </div>
@@ -48,11 +42,8 @@
     <input type="checkbox" id="my-modal-5" class="modal-toggle" />
     <div class="modal">
         <div class="modal-box w-11/12 max-w-5xl">
-            <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-            <div class="modal-action">
-                <label for="my-modal-5" class="btn">Yay!</label>
-            </div>
+            <img :src="previewImage" />
+            <label for="my-modal-5" class="btn btn-ghost">close modal</label>
         </div>
     </div>
 </template>
@@ -66,6 +57,7 @@ export default {
     },
     data() {
         return {
+            previewImage: '',
             "events": [
                 {
                     "name": "Event 1",
