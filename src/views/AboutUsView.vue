c32d63f38c94666c7409820f27f51dc2b3539e8a
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-04-03 10:20:55 +0200

    Update AboutUs

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,23 +1,32 @@
 <template>
     <h1 class="text-4xl font-bold mb-4">Vorstand</h1>
-    <ul class="board">
-        <li v-for="person in persons" :key="person.name">
-            <div class="card card-side bg-base-100 shadow-xl">
-                <figure>
-                    <div :style="{ backgroundImage: `url(${person.image})` }" class="image">
-                    </div>
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title">{{ person.name }}</h2>
-                    <h3>{{ person.function }}</h3>
-                    <div class="card-actions justify-end">
-                        <a class="tel link link-info" v-if="person.tel" :href="person.tel">Anrufen </a>
-                        <a class="email link link-info" v-if="person.email" :href="person.email">Email</a>
-                    </div>
+    <div class="board flex flex-wrap justify-between gap-[25px]">
+        <div v-for="person in persons" class="card card-side bg-base-200 shadow-xl ">
+            <figure><img class="h-[300px] w-[230px]" :src="person.image" alt="Movie" /></figure>
+            <div class="card-body w-[240px]">
+                <h2 class="card-title">{{ person.name }}</h2>
+                <p class="grow-0">{{ person.function }}</p>
+                <div class="flex flex-row gap-[20px] mt-[24px]">
+                    <a :href="'tel:' + person.tel">
+                        <svg class="fill-current" width="24" height="24" viewBox="0 0 32 32"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M11.748 5.773S11.418 5 10.914 5c-.496 0-.754.229-.926.387S6.938 7.91 6.938 7.91s-.837.731-.773 2.106c.054 1.375.323 3.332 1.719 6.058 1.386 2.72 4.855 6.876 7.047 8.337 0 0 2.031 1.558 3.921 2.191.549.173 1.647.398 1.903.398.26 0 .719 0 1.246-.385.536-.389 3.543-2.807 3.543-2.807s.736-.665-.119-1.438c-.859-.773-3.467-2.492-4.025-2.944-.559-.459-1.355-.257-1.699.054-.343.313-.956.828-1.031.893-.112.086-.419.365-.763.226-.438-.173-2.234-1.148-3.899-3.426-1.655-2.276-1.837-3.02-2.084-3.824a.56.56 0 0 1 .225-.657c.248-.172 1.161-.933 1.161-.933s.591-.583.344-1.27-1.906-4.716-1.906-4.716z" />
+                        </svg>
+                    </a>
+                    <a :href="'mailto:' + person.email">
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960"
+                            width="24">
+                            <path
+                                d="M142.152 901.978q-27.599 0-47.865-20.265-20.265-20.266-20.265-47.865V318.152q0-27.697 20.265-48.033 20.266-20.337 47.865-20.337h675.696q27.697 0 48.033 20.337 20.337 20.336 20.337 48.033v515.696q0 27.599-20.337 47.865-20.336 20.265-48.033 20.265H142.152ZM480 601.652l-337.848-223v455.196h675.696V378.652L480 601.652Zm0-65.5 333.848-218H147.152l332.848 218Zm-337.848-157.5v-60.5 515.696-455.196Z" />
+                        </svg>
+                    </a>
                 </div>
+
+
             </div>
-        </li>
-    </ul>
+        </div>
+    </div>
     <h1 class="text-4xl font-bold mt-4 mb-3">VIPs – Funktionäre und Ehrenmitglieder</h1>
     <ul class="vip">
         <li v-for="vip in vips" :key="vip.name">
@@ -52,41 +61,11 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-.board {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    row-gap: 25px;
-}
+@media (max-width: 500px) {
+    .card {
+        width: 100%;
+        height: 250px;
+    }
 
-.board li {
-    width: 40%;
-    height: 250px;
-    min-width: 300px;
-}
-
-.card-actions {
-    display: flex;
-    flex-direction: column;
-}
-
-.image {
-    width: 180px;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    height: 250px;
-    object-fit: cover;
-}
-
-.vip {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    row-gap: 25px;
-}
-
-.vip>li>.card {
-    width: 100%;
 }
 </style>
