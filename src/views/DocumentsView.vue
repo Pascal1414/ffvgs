d6ad1ae7e65356b739dddb2768645cafecc4fb43
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-04-04 10:00:04 +0200

    Update DocumentsView

--- src/views/DocumentsView.vue
+++ src/views/DocumentsView.vue
@@ -1,10 +1,27 @@
 <template>
-    <h1>Downloads</h1>
-    <p>Folgende Dokumente stehen zum Download zur verfügung:</p>
-    <div>
-        <a href="documents/BenutzungsordnungVereinsboot.pdf">Benutzungsordnung Vereinsboot</a>
-        <a href="documents/FFV_Statuten_5_A5.pdf">FFV Statuten 5 A5</a>
-    </div>
+    <h1 class="text-4xl font-bold mb-4">Downloads</h1>
+    <ul class="">
+        <li>
+        </li>
+        <li>
+        </li>
+    </ul>
+    <ul class="menu bg-base-200 w-56 p-2 rounded-box">
+        <li class="menu-title">
+            <span>Vereinsboot</span>
+        </li>
+        <li>
+            <a class="link no-underline" href="documents/BenutzungsordnungVereinsboot.pdf">Benutzungsordnung Vereinsboot</a>
+        </li>
+
+        <li class="menu-title">
+            <span>Statuten</span>
+        </li>
+        <li>
+            <a class="link no-underline" href="documents/FFV_Statuten_5_A5.pdf">FFV Statuten 5 A5</a>
+
+        </li>
+    </ul>
 </template>
 <script lang="ts">
 export default {
