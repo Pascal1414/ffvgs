e9b097b41303f2a1d5d05bc4710a11bad3ac541b
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-04-17 14:54:55 +0200

    Update ProgrammView

--- src/views/ProgrammView.vue
+++ src/views/ProgrammView.vue
@@ -29,8 +29,16 @@
             </div>
         </li>
     </ul>
-
-    <p class="mt-4">Zudem können spontane Anlässe dazukommen. Diese werden per Mail oder im Internet angekündigt.</p>
+    <div class="alert alert-info shadow-lg mt-4">
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                class="stroke-current flex-shrink-0 w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+            <span>Zudem können spontane Anlässe dazukommen. Diese werden per Mail oder im Internet angekündigt.</span>
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
