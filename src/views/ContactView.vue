005b8eb7adb8d33afeb10bba219a465a62bad225
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-04-19 11:24:26 +0200

    Update ContactView

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,4 +1,26 @@
 <template>
+    <form name="contact" method="POST" data-netlify="true">
+        <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <label>Your Role: <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                </select></label>
+        </p>
+        <p>
+            <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
+    <hr>
+    <hr>
     <div class="card w-[100%] bg-base-200 shadow-xl mb-4">
         <div class="card-body">
             <h1 class="text-2xl font-bold">Kontakt</h1>
