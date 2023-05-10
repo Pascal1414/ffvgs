7ac91f09b101fb58ba9bdcb341411c0ab9872101
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-05-10 10:01:49 +0200

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,25 @@
         </div>
     </div>
 
+
+
+    <div class="divider"></div>
+    <div class="sm:hero min-h-[400px] bg-base-100">
+        <div class="hero-content w-full flex-col lg:flex-row-reverse">
+            <img src="/images/event/jungfischerkurs/fischknusperli.jpg"
+                class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
+            <div class="w-full sm:w-auto">
+                <h1 class="text-5xl font-bold">Jungfischerkurs </h1>
+                <p class="py-6">Vielen Dank für die zahlreichen Anmeldungen für den Jungfischerkurs.
+                    Während beiden Tagen läuft auch unsere Fischküche auf der Schlosswiese auf Hochtouren.
+                    Zanderfilets-«Chnusperli», aber auch Allerlei vom Grill stehen am Samstag ab 11 Uhr bis ca. 16 Uhr zur
+                    Auswahl (Grill und Getränke bis ca. 18 Uhr). Es findet kein Abendbetrieb statt (dafür dann am
+                    Sommernachtsfest vom 12./13. August). Am Sonntag ist unsere Küche ab 11 offen – «es hät solangs hät» –
+                    bis spätestens 16 Uhr. </p>
+                <router-link class="btn btn-primary" to="/event/jungfischerkurs">Mehr erfahren</router-link>
+            </div>
+        </div>
+    </div>
     <div class="divider"></div>
 
     <div class="sm:hero min-h-[400px] bg-base-100">
@@ -28,22 +47,6 @@
             </div>
         </div>
     </div>
-
-    <div class="divider"></div>
-    <div class="sm:hero min-h-[400px] bg-base-100">
-        <div class="hero-content w-full flex-col lg:flex-row-reverse">
-            <img src="/images/event/jungfischerkurs/fischen2.jpg" class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
-            <div class="w-full sm:w-auto">
-                <h1 class="text-5xl font-bold">Jungfischerkurs </h1>
-                <p class="py-6">Vielen Dank für die zahlreichen Anmeldungen für den Jungfischerkurs. </p>
-                <router-link class="btn btn-primary" to="/event/jungfischerkurs">Mehr erfahren</router-link>
-                <!--p class="py-6"> In diesem Jahr bietet die FFVGS wieder 50 Jugendlichen ab Jahrgang 2011 bis 2015 die
-                    Möglichkeit, unter professioneller Anleitung einen Einblick in die Freizeit-Angelei zu erhalten.</p>
-                <router-link class="btn btn-primary" to="/event/jungfischerkurs">Jetzt anmelden</router-link-->
-            </div>
-        </div>
-    </div>
-
     <div class="divider"></div>
 
     <div class="sm:hero min-h-[400px] bg-base-100">
