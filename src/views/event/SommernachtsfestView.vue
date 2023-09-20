a2df81a5ceed36e4358273b0694950cb10c3d8c7
Pascal Rieder <pascal.rider@edu.tbz.ch>
2023-09-20 13:59:44 +0200

    Update Sommernachtsfest

--- src/views/event/SommernachtsfestView.vue
+++ src/views/event/SommernachtsfestView.vue
@@ -1,22 +1,37 @@
 <template>
   <h1 class="text-4xl font-bold mb-4">Sommernachtsfest</h1>
-  <div class="grid card bg-base-200 rounded-box p-4 gap-1 mb-4">
-    <p>
-      Am Samstag öffnen wir unsere Fischküche, an deren Theke feine Zanderfilets im Bierteig
-      gebacken werden. Sie sind in der Regel bis 22 Uhr erhältlich. Auch am Sonntag gibt es die
-      zarten Fischfilets, so lange, bis wir ausverkauft sind.
-    </p>
-    <p>
-      Für jene Gäste, die lieber Grilladen geniessen, gibt es Würste oder Steaks vom Grill. Auch
-      Pommes Frites werden frisch zubereitet. Dazu bietet unsere Theke im Festzelt vielerlei
-      passende Getränke sowie Kaffee.
-    </p>
-    <p>
-      Weil ein sommerliches Fest am See eine fröhliche Sache ist, sorgen wir auch für musikalische
-      Unterhaltung. Am Samstagabend spielt eine Band zum Tanz auf, am Sonntagvormittag ab 11 Uhr
-      findet das Frühschoppenkonzert satt.
-    </p>
-    <p>Wir freuen uns auf zahlreiche Besucherinnen und Besucher, die mit uns feiern!</p>
+  <div class="flex flex-col w-full lg:flex-row">
+    <div class="grid lg:w-[50%] flex-grow  card bg-base-300 rounded-box place-items-center">
+
+      <div class="card  text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Samstag</h2>
+          <p>Am Samstag öffnen wir unsere Fischküche, an deren Theke feine Zanderfilets im Bierteig gebacken werden. Sie
+            sind in der Regel bis 22 Uhr erhältlich. Für jene Gäste, die lieber Grilladen geniessen, gibt es Würste oder
+            Steaks vom Grill. Auch Pommes Frites werden frisch zubereitet. Dazu bietet unsere Theke im Festzelt vielerlei
+            passende Getränke sowie Kaffee.</p>
+          <p>
+            Weil ein sommerliches Fest am See eine fröhliche Sache ist, sorgen wir auch für musikalische Unterhaltung. Am
+            Abend spielt eine Band zum Tanz auf.
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="divider lg:divider-horizontal"></div>
+
+    <div class="grid lg:w-[50%] flex-grow card bg-base-300 rounded-box place-items-center">
+      <div class="card  text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Sonnatg</h2>
+          <p>Auch am Sonntag gibt es die zarten Fischfilets und die Grilladen, so lange, bis wir ausverkauft sind. </p>
+          <p> Am Sonntagvormittag ab 11 Uhr findet das Frühschoppenkonzert satt.</p>
+
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 <script lang="ts">
