eb7ee410551f12a0a1c4dee984bc84e02f433219
Pascal <pascal.rieder@outlook.com>
2024-01-21 17:06:46 +0100

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,37 +13,21 @@
     </div>
   </div>
 
-  <div class="divider"></div>
-  <div class="sm:hero min-h-[400px] bg-base-100">
-    <div class="hero-content w-full flex-col lg:flex-row-reverse">
-      <div>
-        <h1 class="text-5xl font-bold">Neujahrsapéro 2024</h1>
-        <p class="py-6">
-          Wir sind am 6. Januar 2024 um 11:30 vom Sportfischer Verein Glattal zum Neujahrsapéro eingeladen. Der Apéro
-          wird auf dem Vereinsgrundstück des SFV Glattal in Fällanden stattfinden. Parkplätze gibt es vis-à-vis der
-          Jugendherberge Fällanden. Anmeldungen bitte bis am 28. Dezember 2023 an <a href="mailto:rider@renaterieder.ch"
-            class="link link-info">rieder@renaterieder.ch</a> oder per Telefon an 078 624 52 07.
-        </p>
-      </div>
-      <img src="/images/home/fireworks.jpg" class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
-    </div>
-  </div>
   <div class="divider">
   </div>
 
   <div class="sm:hero min-h-[400px] bg-base-100">
     <div class="hero-content w-full flex-col lg:flex-row-reverse">
-      <img src="/images/home/fisch-des-jahres-2023.jpg" class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
+      <img src="public\images\home\marmorata-forelle.png" class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
       <div>
         <h1 class="text-5xl font-bold">Fisch des Jahres</h1>
         <p class="py-6">
-          Der Fisch, der sich gegen andere durchsetzt und sich veränderten Lebensbedingungen
-          anpassen kann, wurde vom Schweizerischen Fischerei-Verband zum Fisch des Jahres 2023
-          gewählt: Der Hecht. Einer der fischereilich beliebtesten und attraktivsten Fische im
-          Lande. Trotz veränderter Lebebedingungen aufgrund des Siedlungsdrucks, der Verbauung der
-          Seeufer und des Klimawandels geht es dem Hecht heute nicht schlechter als früher. Seine
-          Bestände und Fischerträge sind seit 20 Jahren stabil. Quelle:
-          <a class="link link-info" href="https://sfv-fsp.ch/home/aktuell/?load=1159">SFV</a>
+          Perfekt getarnt beherrschte die Marmorata-Forelle während Jahrtausenden die Gewässer der Südschweiz. Der
+          Schweizerische Fischerei-Verband SFV hat sie zum Fisch des Jahres 2024 gewählt, weil sie Botschafterin der
+          einzigartigen Fischvielfalt der Alpensüdseite ist, jetzt aber um ihr Überleben kämpfen muss.
+          <br>Quelle:
+          <a class="link link-info" href="https://www.youtube.com/watch?v=YYlbwJKJyHg">Video</a> /
+          <a class="link link-info" href="https://sfv-fsp.ch/home/aktuell/?load=1232">SFV</a>
         </p>
       </div>
     </div>
