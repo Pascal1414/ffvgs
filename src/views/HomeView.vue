866bfcff1d3b93a28e5f15590beaf6a4779c7b14
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-02-12 10:38:27 +0100

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,6 +17,48 @@
   </div>
 
   <div class="sm:hero min-h-[400px] bg-base-100">
+    <div class="hero-content w-full flex-col">
+      <h1 class="text-5xl font-bold">2024 wird ein Jahr ohne Jungfischerkurs und Sommernachtsfest-Anlass
+      </h1>
+      <p class="font-bold">
+        Die Spatzen pfeifen es seit einer Weile schon von den Dächern: Der lokale Fischerverein verzichtet
+        dieses Jahr auf die Durchführung seiner grossen Anlässe wie den Jungfischerkurs am MuttertagsWochenende und das
+        Sommernachtsfest im August.</p>
+      <p>
+        Beide Anlässe erfreuen sich recht grosser Beliebtheit bei den Besuchern, besonders natürlich bei
+        guter Witterung. Trotzdem wird es immer schwieriger für den Verein, die beiden Anlässe zu
+        stemmen. Die Gründe dafür sind ähnlich gelagert bei anderen Vereinen: Helfer-Mangel, immer
+        striktere Auflagen durch Kanton und Gemeinde verbunden mit einem enormen administrativen
+        Aufwand und Verantwortung sowie ein erhebliches finanzielles Risiko (hier spielt häufig das Wetter
+        die entscheidende Rolle). Allein die Zeltkosten am Sommernachtsfest belasten die Rechnung jährlich
+        mit rund 15‘000 Franken. Zusätzlich besteht aktuell ein Mangel an Vorstandsmitgliedern, sprich an
+        Personen, die sich verbindlich und tatkräftig für den Verein einsetzen können und wollen. So kann ein
+        Jahresprogramm nicht in gewohntem Rahmen durchgeführt und gestaltet werden. Die
+        Generalversammlung vom 9. Februar hat deshalb dem Antrag des Vorstands auf eine Pause
+        zugestimmt.
+      </p>
+      <p>
+        Wir bedauern diese Tatsache sehr. Der Vorstand nimmt sich nun Zeit, um eine sogenannte
+        Neuausrichtung des Vereins zu planen. Wie gross sollen die Veranstaltungen noch sein, sind sie noch
+        zeitgemäss, wo finden sich Helfer? Wie können die Mitglieder besser eingebunden werden? Wie soll
+        künftig das Jahresprogramm grundsätzlich aussehen? Wer kann was und wann der anstehenden
+        Arbeiten übernehmen? Wie können Kosten (z.Bsp. Zelt, Sicherheit, sanitäre Anlagen etc.) gesenkt
+        werden? Das alles sind Fragen, denen sich der Verein und sein Vorstand in den nächsten Monaten
+        widmen werden; dafür gibt es auch Arbeitsgruppen, die nach Thema mögliche Lösungen suchen
+        werden.
+      </p>
+      <p>
+        Wir sind sehr optimistisch gestimmt, dass auch ein grosser Verein wie der lokale Fischerverein nichts
+        von seiner Attraktivität und Flexibilität verlieren muss, wenn die Weichen zur richtigen Zeit optimal
+        gestellt werden!
+      </p>
+    </div>
+  </div>
+
+  <div class="divider">
+  </div>
+
+  <div class="sm:hero min-h-[400px] bg-base-100">
     <div class="hero-content w-full flex-col lg:flex-row-reverse">
       <img src="\images\home\marmorata-forelle.png" class="w-full sm:w-auto max-w-sm rounded-lg shadow-2xl" />
       <div>
