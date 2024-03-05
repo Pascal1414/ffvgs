ea69c1e4a535c7dcbdc40b923228295417a19c88
Pascal <pascal.rieder@outlook.com>
2024-03-05 19:12:10 +0100

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,5 +3,5 @@ import type { Database } from './database/supabase'
 
 export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLIC_ANON_KEY
+  import.meta.env.VITE_SUPABASE_KEY
 )
