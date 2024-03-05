6619caebc41ffc94dff729dbfb4bb36be53e9229
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-05 11:04:10 +0100

    Update supabase-policies.sql

--- database/supabase-policies.sql
+++ database/supabase-policies.sql
@@ -2,5 +2,5 @@
 CREATE POLICY "Enable read access for all users" ON "public"."Program" AS PERMISSIVE FOR
 SELECT TO public USING (true);
 /* Enable read, insert, update and delete access for authenticated users only */
-CREATE POLICY "Enable everything for authenticated users only" ON "public"."Program" AS PERMISSIVE FOR ALL TO authenticated WITH CHECK (auth.uid() = user_id);
+CREATE POLICY "Enable everything for authenticated users only" ON "public"."Program" AS PERMISSIVE FOR ALL TO authenticated USING (true);
 ;
