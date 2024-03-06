03480801fa7488eff5ad8a0baab1a9c1afbc505c
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-06 11:07:50 +0100

    Update HeroImage

--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -2,7 +2,10 @@
     <div class="hero min-h-[30rem]">
         <div class="hero-content flex-col w-full  text-center lg:text-start lg:gap-12"
             :class="{ 'lg:flex-row-reverse': reversed, 'lg:flex-row': !reversed, 'lg:justify-between': reversed, 'lg:justify-start': !reversed }">
-            <img :src="props.images?.[0]" class="max-w-sm rounded-lg shadow-2xl" />
+            <img v-if="props.images?.length == 1" :src="props.images?.[0]" class="max-w-sm rounded-lg shadow-2xl" />
+            <div v-else class="w-full lg:w-1/2">
+                <Slideshow :images="getImagesForSlideshow()" />
+            </div>
             <div>
                 <slot />
             </div>
@@ -11,6 +14,8 @@
 </template>
 
 <script lang="ts" setup>
+import Slideshow from './Slideshow.vue'
+
 const props = defineProps({
     'images': {
         type: Array<string>,
@@ -21,6 +26,15 @@ const props = defineProps({
         default: false
     }
 })
+
+function getImagesForSlideshow() {
+    return props.images.map((image: string) => {
+        return {
+            src: image,
+            alt: 'Hero image'
+        }
+    })
+}
 
 </script>
 
--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="carousel w-full">
       <div v-for="(image, index) in images" :key="index" :id="index.toString()" class="carousel-item w-full">
-        <img :src="image.src" class="w-full" />
+        <img :src="image.src" :alt="image.alt" class="w-full" />
       </div>
     </div>
     <div class="flex justify-center w-full py-2 gap-2">
@@ -11,15 +11,12 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'SlideShow',
-  props: {
-    images: {
-      type: Array as () => Array<{ src: string; alt: string }>,
-      required: true
-    }
-  },
-  methods: {}
-}
+<script lang="ts" setup>
+const props = defineProps({
+  'images': {
+    type: Array<{ src: string, alt: string }>,
+    required: true
+  }
+})
+
 </script>
