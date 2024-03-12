5e1c60df6cc37196e355f84dff5466a3e3b36931
Pascal <pascal.rieder@outlook.com>
2024-03-12 11:20:06 +0100

    Update to nuxt
    The app now uses nuxt and no longer vue

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hero min-h-[300px] bg-base-100">
+    <div class="hero-content text-center">
+      <div class="max-w-xl">
+        <h1 class="text-5xl font-bold">FFVGS</h1>
+        <p class="py-6">
+          Der Freie Fischerverein Greifensee-Schwerzenbach besteht seit 1978 und ist ein Verein mit
+          rund 170 Mitgliedern aus allen Altersklassen. Wir fördern den Angelsport und geben unser
+          Wissen den jüngeren Fischern weiter. So lernen Sie aus professioneller Sicht, wie die
+          Fischerei Spass macht und sich trotzdem mit Flora und Fauna am Wasser vereinbaren lässt.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div v-for="(article, index) in  articles ">
+    <div class="divider" />
+    <ImageHero v-if="article.images?.length" :reversed="index % 2 == 0" :images="article.images">
+      <div class="marked" v-html="marked(article.text)" />
+    </ImageHero>
+    <div v-else>
+      <div class="sm:hero min-h-[400px]">
+        <div class="hero-content place-items-start w-full flex-col marked" v-html="marked(article.text)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import type { Ref } from 'vue';
+import { marked } from 'marked';
+import ImageHero from '@/components/ImageHero.vue';
+
+let articles = ref([])
+
+
+onMounted(() => {
+});
+
+</script>
+
+<style scoped>
+@import url("~/assets/css/marked.css");
+</style>
---
+++ plugins/after-each-navigation.ts
@@ -0,0 +1,6 @@
+export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
+  /*nuxtApp.router.afterEach((to, from) => {
+    const drawer = document.getElementById("my-drawer-3") as HTMLInputElement;
+    drawer.checked = false;
+  });*/
+});
