5c03b2210059649ca7833ceb4975b6601ac1e919
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-18 14:12:13 +0100

    Updat home-article

--- types/home-article.ts
+++ types/home-article.ts
@@ -1,66 +1,10 @@
+import type { ResImage } from "./image";
+
 export type HomeArticle = {
   id: number;
   Text: string;
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
-  Images: [
-    data: [
-      {
-        id: number;
-        attributes: {
-          name: string;
-          alternativeText: string | null;
-          caption: string | null;
-          width: number;
-          height: number;
-          formats: {
-            small: {
-              ext: string;
-              url: string;
-              hash: string;
-              mime: string;
-              name: string;
-              path: string | null;
-              size: number;
-              width: number;
-              height: number;
-            };
-            medium: {
-              ext: string;
-              url: string;
-              hash: string;
-              mime: string;
-              name: string;
-              path: string | null;
-              size: number;
-              width: number;
-              height: number;
-            };
-            thumbnail: {
-              ext: string;
-              url: string;
-              hash: string;
-              mime: string;
-              name: string;
-              path: string | null;
-              size: number;
-              width: number;
-              height: number;
-            };
-          };
-          hash: string;
-          ext: string;
-          mime: string;
-          size: number;
-          url: string;
-          previewUrl: string | null;
-          provider: string;
-          provider_metadata: any | null;
-          createdAt: string;
-          updatedAt: string;
-        };
-      }
-    ]
-  ];
+  Images: ResImage[];
 };
