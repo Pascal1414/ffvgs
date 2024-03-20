2f335757c93677734f69766683e3f23bc3673547
Pascal <pascal.rieder@outlook.com>
2024-03-20 22:42:49 +0100

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-      <p class="font-semibold text-primary">404</p>
-      <h1 class="text-4xl font-bold">Seite nicht gefunden</h1>
-      <p class="mt-6">Die Seite nach der du suchst wurde nicht gefunden.</p>
+      <p class="font-semibold text-primary">{{ error?.statusCode }}</p>
+      <h1 class="text-4xl font-bold">{{ error?.statusMessage }}</h1>
+      <p class="mt-6">{{ error?.message }}</p>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
+const error = useError()
+
 </script>
 
 <style lang="css">
