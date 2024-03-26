22a7df26caea59e694930cee4865ec21d6618e98
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-26 15:11:06 +0100

    Update Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -80,6 +80,28 @@
 </template>
 
 <script lang="ts" setup>
+let themeCheckbox = ref(true)
+
+onMounted(() => {
+  const theme = localStorage.getItem('theme')
+  if (theme === 'light') {
+    updateTheme(false)
+  } else {
+    updateTheme(true)
+  }
+})
+
+function updateTheme(isDarkMode: boolean) {
+  if (isDarkMode) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+    themeCheckbox.value = false
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+    themeCheckbox.value = true
+  }
+}
 
 </script>
 
