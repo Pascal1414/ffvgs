63e62daf85d854f601080547011272988c05c39c
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-26 16:19:10 +0100

    Create type vip

---
+++ types/vip.ts
@@ -0,0 +1,11 @@
+import type { ResImage } from "./image";
+
+export type Vip = {
+  id: number;
+  Name: string;
+  Function: string;
+  Image: ResImage;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+};
