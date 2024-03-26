3239dbe0636309553df3de464146baeb59772157
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-26 14:32:13 +0100

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBar>
+    <main class="mx-auto w-[65%] mt-4 mb-4">
+      <slot></slot>
+    </main>
+
+    <Footer />
+  </NavBar>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
