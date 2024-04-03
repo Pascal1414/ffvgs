9f0d2196729aa719bba624b3270870b4be0865e9
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-04-03 12:11:47 +0200

    Update Galery
    Add Cloudinary

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,11 @@ export default defineNuxtConfig({
       backendUrl: process.env.STRAPI_API_URL,
       apiUrl: process.env.STRAPI_API_URL + "/api",
       uploadsUrl: process.env.STRAPI_API_URL + "/uploads",
+      cloudinary: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+      },
     },
   },
 });
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-for="(event, index) in events" :key="index">
+  <!--div v-for="(event, index) in events" :key="index">
     <div class="card card-compact w-[100%] bg-base-200 shadow-xl mb-4">
       <div class="card-body">
         <h1 class="text-2xl font-bold mb-2">{{ event.name }}</h1>
@@ -49,12 +49,28 @@
         </svg>
       </label>
     </div>
-  </div>
+  </div-->
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import type { Ref } from 'vue'
+import cloudinary from 'cloudinary'
+
+const config = useRuntimeConfig()
+
+cloudinary.v2.config({
+  cloud_name: config.public.cloudinary.cloudName,
+  api_key: config.public.cloudinary.apiKey,
+  api_secret: config.public.cloudinary.apiSecret
+});
+
+cloudinary.v2.search
+  .expression('galery*')
+  .sort_by('created_at', 'desc')
+  .execute()
+  .then(console.log).catch(console.error)
+
 
 const previewImageArray = ref([
   { url: '', caption: '' }
@@ -72,235 +88,6 @@ function next() {
 }
 
 
-const events = ref([
-  {
-    name: 'Helferausflug (30.09.2023)',
-    images: [
-      [
-        {
-          url: '/images/galery/Helferausflug23/traktorfahren_small.jpg',
-          caption: 'Traktorfahren'
-        },
-        {
-          url: '/images/galery/Helferausflug23/traktorfahren2_small.jpg',
-          caption: 'Traktorfahren'
-        }
-        , {
-          url: '/images/galery/Helferausflug23/traktorfahren3_small.jpg',
-          caption: 'Traktorfahren'
-        }
-      ], [
-        {
-          url: '/images/galery/Helferausflug23/armbrustschiessen_small.jpg',
-          caption: 'Armbrustschiessen'
-        },
-        {
-          url: '/images/galery/Helferausflug23/melken_small.jpg',
-          caption: 'Melken'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Sommernachtsfest (12. / 13.08.2023)',
-    images: [
-      [
-        {
-          url: '/images/galery/Sommernachtsfest23/fischkueche_small.jpg',
-          caption: 'Fischküche'
-        },
-        {
-          url: '/images/galery/Sommernachtsfest23/saucen_herstellen_small.jpg',
-          caption: 'Saucen herstellen'
-        }
-      ]
-    ],
-  },
-  {
-    name: '2. Vereinsfischen (01.07.2023)',
-    images: [
-
-      [
-        {
-          url: '/images/galery/2.Vereinsfischen23/grosse_fänge_small.jpg',
-          caption: 'Tagessieger'
-        },
-        {
-          url: '/images/galery/2.Vereinsfischen23/mittagessen_small.jpg',
-          caption: 'Mittagessen'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/2.Vereinsfischen23/felche_small.jpg',
-          caption: 'Grösste Felche'
-        },
-        {
-          url: '/images/galery/2.Vereinsfischen23/felche_messen_small.jpg',
-          caption: 'Die grösste Felche wird gemessen'
-        }
-      ]
-    ]
-  },
-  {
-    name: '1. Vereinsfischen (01.05.2023)',
-    images: [
-      [
-        {
-          url: '/images/galery/1.Vereinsfischen23/mittagessen.jpg',
-          caption: 'Mittagessen'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/1.Vereinsfischen23/hecht.jpg',
-          caption: 'Gefangener Hecht '
-        },
-        {
-          url: '/images/galery/1.Vereinsfischen23/hecht-jugend.jpg',
-          caption: 'Gefangene Hechte'
-        },
-        {
-          url: '/images/galery/1.Vereinsfischen23/essen.jpg',
-          caption: 'Mittagessen in der Milchhütte'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Absenden (19.11.2022)',
-    images: [
-      [
-        {
-          url: '/images/galery/Absenden22/20221119_200447small.jpg',
-          caption: 'Absenden'
-        },
-        {
-          url: '/images/galery/Absenden22/20221119_230733small.jpg',
-          caption: 'Boot'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/Absenden22/absenden1small.jpg',
-          caption: 'Abendessen'
-        },
-        {
-          url: '/images/galery/Absenden22/absenden2small.jpg',
-          caption: 'Lotto'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/Absenden22/absenden3small.jpg',
-          caption: 'Dessert'
-        },
-        {
-          url: '/images/galery/Absenden22/absenden4small.jpg',
-          caption: 'Boot'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Trüschenfischen (05.11.2022)',
-    images: [
-      [
-        {
-          url: '/images/galery/Trüschenfischen22/IMG_20221105_081130844small.jpg',
-          caption: 'Schiffe'
-        },
-        {
-          url: '/images/galery/Trüschenfischen22/IMG_20221105_111110798small.jpg',
-          caption: 'Unwetter'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/Trüschenfischen22/IMG_20221105_111112339small.jpg',
-          caption: 'Regen'
-        },
-        {
-          url: '/images/galery/Trüschenfischen22/IMG_20221105_141223498small.jpg',
-          caption: 'Regenbogen'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Silchsammelaktion (22.10.2022)',
-    images: [
-      [
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_142359small.jpg',
-          caption: 'Taucher springt ins Wasser'
-        },
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_142717small.jpg',
-          caption: 'Taucher zieht Flossen an'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_134846small.jpg',
-          caption: 'Anweisungen'
-        },
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_141214small.jpg',
-          caption: 'Taucher zieht sich Ausrüstung an.'
-        },
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_142011small.jpg',
-          caption: 'Boot wird bereit gemacht'
-        }
-      ],
-      [
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_133154small.jpg',
-          caption: ''
-        },
-        {
-          url: '/images/galery/Silchsammelaktion22/20221022_142203small.jpg',
-          caption: 'Es geht los'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Helferausflug Brauerei Wädenswil (17.09.2022)',
-    images: [
-      [
-        {
-          url: '/images/galery/Helferausflug22/IMG_0868small.jpg',
-          caption: 'Schiff vom Zürichsee'
-        },
-        {
-          url: '/images/galery/Helferausflug22/IMG_0883small.jpg',
-          caption: 'In der Brauerei'
-        },
-        {
-          url: '/images/galery/Helferausflug22/IMG_0885small.jpg',
-          caption: 'Auf dem Zürichsee'
-        }
-      ]
-    ]
-  },
-  {
-    name: 'Glattfischen (10.09.2022)',
-    images: [
-      [
-        {
-          url: '/images/galery/Glattfischen22/IMG_5240small.jpg',
-          caption: 'Die Fänge'
-        },
-        {
-          url: '/images/galery/Glattfischen22/IMG_5243small.jpg',
-          caption: 'Die Fänge'
-        }
-      ]
-    ]
-  }
-])
 </script>
 
 <style scoped></style>
