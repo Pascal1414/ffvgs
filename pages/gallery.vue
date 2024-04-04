43be527ed994dcc1b6bef4343a38d5ca613469c4
Pascal <pascal.rieder@outlook.com>
2024-04-04 10:55:28 +0200

    Update Gallery

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -8,51 +8,27 @@
       <div class="card-body">
         <h1 class="text-2xl font-bold mb-2">{{ galeryItem.name }} ({{ galeryItem.date }})</h1>
         <div class="grid grid-cols-3 gap-4">
-          <label v-for="(image, index) in galeryItem.images" for="my-modal-5" :key="index" @click="() => {/*
-    previewImageArray = []
-  galeryItem.images.forEach((group) => {
-      group.forEach((image) => {
-        previewImageArray.push(image)
-      })
-    })
-    previewImageIndex = index*/
-    }
-      ">
+          <div v-for="(image, index) in galeryItem.images" :key="index"
+            @click="openPreviewModal(galeryItem.images, index)">
             <img class="md:rounded-md" :src="image.url" :alt="image.caption" />
-          </label>
+          </div>
         </div>
       </div>
     </div>
   </div>
 
-  <input type="checkbox" id="my-modal-5" class="modal-toggle" />
-  <div class="modal">
-    <div class="modal-box max-w-[100%] w-[100%] flex row h-[100%] bg-transparent">
-      <button class="btn btn-square btn-primary absolute top-[50%] !translate-y-[-50%] left-[2%]" @click="previous()">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left"
-          viewBox="0 0 16 16">
-          <path fill-rule="evenodd"
-            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-        </svg>
-      </button>
-      <img class="object-contain ml-auto mr-auto max-w-[80%]" v-if="previewImageArray[previewImageIndex]"
-        :src="previewImageArray[previewImageIndex].url" />
-      <button class="btn btn-square btn-primary absolute top-[50%] !translate-y-[-50%] right-[2%]" @click="next()">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right"
-          viewBox="0 0 16 16">
-          <path fill-rule="evenodd"
-            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-        </svg>
-      </button>
-      <label for="my-modal-5" class="btn btn-square btn-primary absolute top-[10px] right-[2%]">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
-          viewBox="0 0 16 16">
-          <path
-            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-        </svg>
-      </label>
+  <dialog ref="preview_modal" class="modal">
+    <div class="modal-box max-h-none max-w-none w-[90%] h-[80%]">
+      <img class="md:rounded-md" :src="previewImages[previewImageIndex]?.url"
+        :alt="previewImages[previewImageIndex]?.caption" />
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
     </div>
-  </div>
+  </dialog>
 </template>
 
 <script lang="ts" setup>
@@ -60,8 +36,10 @@ import { ref } from 'vue'
 import type { AsyncData } from '#app'
 import type { Ref } from 'vue'
 import type { Galery } from '~/types/galery'
+import type { ResImage } from '~/types/image';
 
 const config = useRuntimeConfig()
+
 
 const { data: galeryItems, pending } = await useLazyFetch(config.public.apiUrl + '/galeries', {
   query: { "populate": '*' },
@@ -70,20 +48,17 @@ const { data: galeryItems, pending } = await useLazyFetch(config.public.apiUrl +
   }
 })
 
-const previewImageArray = ref([
-  { url: '', caption: '' }
-])
-const previewImageIndex: Ref<number> = ref(-1)
+/* Image Preview */
+const preview_modal = ref<HTMLDialogElement | null>(null)
+const previewImages = ref<ResImage[]>([])
+const previewImageIndex = ref<number>(0)
 
-function previous() {
-  if (previewImageIndex.value > 0) previewImageIndex.value = previewImageIndex.value - 1
-  else previewImageIndex.value = previewImageArray.value.length - 1
+function openPreviewModal(images: ResImage[], currentIndex: number) {
+  previewImageIndex.value = currentIndex
+  previewImages.value = images
+  preview_modal.value?.showModal()
 }
-function next() {
-  if (previewImageIndex.value < previewImageArray.value.length - 1)
-    previewImageIndex.value = previewImageIndex.value + 1
-  else previewImageIndex.value = 0
-}
+
 
 </script>
 
