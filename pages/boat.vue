32588acd6e286924dd5579d8847c73947ceba0bd
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2024-09-02 11:30:33 +0200

    Update boat.vue

--- pages/boat.vue
+++ pages/boat.vue
@@ -41,6 +41,15 @@
         möglich wieder einsatzbereit zu machen.</span>
     </div>
 
+    <div role="alert" class="alert alert-info">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+      </svg>
+      <span> Wärend dem OpenAir ist das Boot nicht erreichbar und kann nicht verwendet werden.
+      </span>
+    </div>
+
     <div class="grid flex-grow card bg-base-200 rounded-box p-4 w-[100%]">
       <div class="flex row justify-between mb-4">
         <h2 class="text-2xl font-bold mb-2">Tarife:</h2>
