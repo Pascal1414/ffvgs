c2b236349c857c58da6cddfa8fe318e009559c33
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2024-09-03 10:38:52 +0200

    Create JoinForm

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,131 +2,17 @@
 
   <ContactForm id="contact" />
 
-  <div class="card w-[100%] bg-base-200 shadow-xl mb-4">
-    <div class="card-body">
-      <h1 class="text-2xl font-bold">Beitrittserklärung</h1>
-      <p>Sehr gerne möchten wir Sie in unseren Verein aufnehmen.</p>
-      <form name="join-association" method="POST" :action="config.public.formspree.joinFormUrl">
-        <div class="card w-[100%] bg-base-300 shadow-xl">
-          <div class="card-body gap-0">
-            <p class="mb-2">Ich wünsche eine Aufnahme als:</p>
-
-            <div v-for="(membership, index) in memberships" :key="index" class="form-control mb-0">
-              <label class="label cursor-pointer justify-start">
-                <input type="radio" name="membership" class="radio checked:bg-red-500" required
-                  :value="membership.name" />
-                <span class="label-text ml-4">{{
-                  membership.name + ' (' + membership.price + ') ' + membership.age
-                }}</span>
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div class="split-cols grid grid-cols-2 gap-[15px] mb-0 mt-3">
-          <div class="mb-0">
-            <label class="label" for="join-firstname">
-              <span class="label-text">Vorname</span>
-            </label>
-            <input id="join-firstname" name="firstname" type="text" placeholder="Vorname"
-              class="input input-bordered w-full" required />
-
-            <label class="label" for="join-lastname">
-              <span class="label-text">Lastname</span>
-            </label>
-            <input id="join-lastname" name="lastname" type="text" placeholder="Nachname"
-              class="input input-bordered w-full" required />
-
-            <label class="label" for="join-birthdate">
-              <span class="label-text">Geburtsdatum</span>
-            </label>
-            <input id="join-birthdate" name="birthdate" type="date" placeholder="Geburtsdatum"
-              class="input input-bordered w-full" required />
-          </div>
-          <div class="mb-0">
-            <label class="label" for="join-street">
-              <span class="label-text">Strasse und Nummer</span>
-            </label>
-            <input id="join-street" name="street" type="text" placeholder="Strasse und Nummer"
-              class="input input-bordered w-full" required />
-
-            <label class="label" for="join-zip">
-              <span class="label-text">PLZ</span>
-            </label>
-            <input id="join-zip" type="text" name="zip" placeholder="PLZ" class="input input-bordered w-full"
-              required />
-
-            <label class="label" for="join-place">
-              <span class="label-text">Ort</span>
-            </label>
-            <input id="join-place" name="place" type="text" placeholder="Ort" class="input input-bordered w-full"
-              required />
-          </div>
-        </div>
-        <div class="mb-2">
-          <label class="label" for="join-email">
-            <span class="label-text">Email</span>
-          </label>
-          <input id="join-email" name="email" type="email" placeholder="Email" class="input input-bordered w-full"
-            required />
-
-          <label class="label" for="join-message">
-            <span class="label-text">Nachricht</span>
-          </label>
-          <textarea id="join-message" name="message" class="textarea textarea-bordered w-full"
-            placeholder="Nachricht"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary mt-3 max-w-xs">Absenden</button>
-      </form>
-    </div>
-  </div>
+  <JoinForm id="join" />
 
   <EventRegistrationForm id="registration-lungerersee" />
+
 </template>
 
 <script lang="ts" setup>
 import EventRegistrationForm from '~/components/forms/EventRegistrationForm.vue';
 import ContactForm from '~/components/forms/ContactForm.vue';
-import { ref } from 'vue';
-
-const config = useRuntimeConfig();
-
-const memberships = ref([
-  {
-    name: 'Jungfischer',
-    price: '50.00',
-    age: 'bis 14 Jahren'
-  },
-  {
-    name: 'Junior',
-    price: '50.00',
-    age: 'ab 14 Jahren'
-  },
-  {
-    name: 'Aktivmitglied',
-    price: '120.00',
-    age: ''
-  },
-  {
-    name: 'Passivmitglied',
-    price: '50.00',
-    age: ''
-  },
-  {
-    name: 'Veteran',
-    price: '70.00',
-    age: 'Aktivmitglied ab 65 Jahren'
-  }
-])
-
+import JoinForm from '~/components/forms/JoinForm.vue';
 
 </script>
 
-<style scoped>
-@media (max-width: 600px) {
-  .split-cols {
-    @apply grid-cols-1;
-    @apply gap-0;
-  }
-}
-</style>
+<style scoped></style>
