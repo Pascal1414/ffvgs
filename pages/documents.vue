d9ee32cb0544b7f1e3a44b90bf4c44a43237e94c
Pascal Rieder <pascal.rieder@edu.tbz.ch>
2024-09-09 10:59:34 +0200

    Update documents page

--- pages/documents.vue
+++ pages/documents.vue
@@ -9,27 +9,27 @@
       <span>Vereinsboot</span>
     </li>
     <li>
-      <a class="link no-underline" href="/Documents/BenutzungsordnungVereinsboot.pdf">Benutzungsordnung Vereinsboot</a>
+      <a class="link no-underline" href="/documents/BenutzungsordnungVereinsboot.pdf">Benutzungsordnung Vereinsboot</a>
     </li>
 
     <li class="menu-title">
       <span>Statuten</span>
     </li>
     <li>
-      <a class="link no-underline" href="/Documents/FFV_Statuten_5_A5.pdf">FFV Statuten 5 A5</a>
+      <a class="link no-underline" href="/documents/FFV_Statuten_5_A5.pdf">FFV Statuten 5 A5</a>
     </li>
     <li class="menu-title">
       <span>Vereinsfischen</span>
     </li>
     <li>
       <a id="vereisfischen" class="link no-underline"
-        href="/Documents/Information-Vereinsmeisterschaft-2024.pdf">Informationen Vereinsfischen</a>
+        href="/documents/Information-Vereinsmeisterschaft-2024.pdf">Informationen Vereinsfischen</a>
     </li>
     <li class="menu-title">
       <span>Gv Protokoll</span>
     </li>
     <li>
-      <a class="link no-underline" href="/Documents/GV-Protokoll-2024.pdf">Gv Protokoll 2024</a>
+      <a class="link no-underline" href="/documents/GV-Protokoll-2024.pdf">Gv Protokoll 2024</a>
     </li>
   </ul>
 </template>
