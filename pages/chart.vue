3b71ea179b890f7488c7ccc1858c1a0d86a17811
Pascal <pascal.rieder@outlook.com>
2025-02-12 11:58:58 +0100

    Revert "Update chart"
    This reverts commit 152b66d3ebee6fbf962ca23ca5e198bc42797d4f.

--- pages/chart.vue
+++ pages/chart.vue
@@ -46,72 +46,40 @@ var option = ref({
     {
       name: "Felchen",
       type: "bar",
-      emphasis: {
-        focus: "series",
-      },
       data: [5975, 4845, 2943, 3949, 3208, 6250, 9086, 3680, 5033, 2616],
     },
     {
       name: "Hecht",
       type: "bar",
-      stack: "total", // Add stack to make it stacked
-      emphasis: {
-        focus: "series",
-      },
       data: [2106, 2500, 2398, 2604, 2461, 2584, 3301, 2990, 3027, 3446],
     },
     {
       name: "Egli",
       type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [4341, 6585, 6980, 5845, 4868, 6118, 6247, 4728, 4228, 4957],
     },
     {
       name: "Wels",
       type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [562, 478, 642, 558, 1036, 701, 746, 739, 968, 895],
     },
     {
       name: "Rotauge",
       type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [958, 533, 994, 654, 451, 467, 551, 434, 471, 369],
     },
     {
       name: "Karpfen",
       type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [878, 635, 364, 392, 397, 201, 208, 295, 209, 151],
     },
     {
       name: "Übrige",
       type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [1764, 1542, 1141, 1118, 1149, 922, 657, 476, 632, 658],
     },
     {
       name: "Total",
-      type: "bar",
-      stack: "total",
-      emphasis: {
-        focus: "series",
-      },
       data: [
         16584, 17118, 98975, 15120, 13570, 17243, 20796, 13342, 14568, 13092,
       ],
