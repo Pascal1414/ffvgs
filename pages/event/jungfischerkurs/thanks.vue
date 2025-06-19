98f0ac90a1b2c050c7099008143f99e0ef3b1055
Pascal <pascal.rieder@outlook.com>
2025-06-19 15:16:59 +0200

    Create Thanks.vue

---
+++ pages/event/jungfischerkurs/thanks.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="hero bg-base-200 py-20">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Vielen Dank</h1>
+      <p class="py-6">
+        Vielen Dank für ihre Anmeldung. Wir werden ihnen ihre Rechnung in Kürze zusenden. Sobald Sie diese beglichen haben, ist Ihre Anmeldung bestätigt. 
+      </p>
+        <router-link to="/" class="btn btn-primary">Zur Landing Page</router-link>
+    </div>
+  </div>
+</div>
+</template>
+<script lang="ts">
+</script>
