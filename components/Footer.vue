b3700c6de5a43d9fff04abe85ae46033e1efb46c
Pascal <pascal.rieder@outlook.com>
2025-07-07 23:14:25 +0200

    Update Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,14 +22,14 @@
     <nav>
       <h6 class="footer-title">Social</h6>
       <div class="grid grid-cols-2 gap-4">
-        <a href="www.instagram.com/ffv.g.s/profilecard/">
+        <a href="https://www.instagram.com/ffv.g.s/profilecard/">
           <client-only>
             <svg
               class="w-6 h-6 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="28"
+              height="28"
               fill="none"
               viewBox="0 0 24 24"
             >
