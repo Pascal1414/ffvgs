d50161d9ad62d6536a49dc5788161af4ce86a32c
Pascal Rieder <pascal.rieder@outlook.com>
2025-08-17 21:05:52 +0200

    Update Contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,11 +3,11 @@
 
   <JoinForm id="join" />
 
-  <EventRegistrationForm id="registration-lungerersee" />
+  <!--<EventRegistrationForm id="registration-lungerersee" />-->
 </template>
 
 <script lang="ts" setup>
-import EventRegistrationForm from '~/components/forms/EventRegistrationForm.vue';
+//import EventRegistrationForm from '~/components/forms/EventRegistrationForm.vue';
 import ContactForm from '~/components/forms/ContactForm.vue';
 import JoinForm from '~/components/forms/JoinForm.vue';
 </script>
