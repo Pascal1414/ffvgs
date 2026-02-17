25f34a46794100963053a0565f028da2f85bfd1e
Pascal <pascal.rieder@outlook.com>
2026-02-17 10:38:22 +0100

    feat(Sponsor): update css

--- components/Sponsor.vue
+++ components/Sponsor.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hover-3d">
     <!-- content -->
-    <figure class="w-50 h-fit rounded-2xl">
-      <img :src="image" :alt="name" />
+    <figure class="h-40 w-60 p-6 rounded-2xl flex  items-center">
+      <img :src="image" :alt="name" class="w-full" />
     </figure>
     <!-- 8 empty divs needed for the 3D effect -->
     <div></div>
