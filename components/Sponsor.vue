db8be2de35eed59ae8be4b3b6529d1100dc8386c
Pascal Rieder <pascal.rieder@outlook.com>
2026-03-17 17:24:34 +0100

    fix(Sponsor): update theme selector

--- components/Sponsor.vue
+++ components/Sponsor.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="hover-3d">
     <!-- content -->
-    <figure class="h-40 w-60 p-6 rounded-2xl flex  items-center">
+    <figure class="h-40 w-60 p-6 rounded-2xl flex items-center">
       <img
         :src="image"
         :alt="name"
-        class="w-full"
-        :class="{ 'dark:hidden': darkModeImage }"
+        :class="['w-full', darkModeImage && 'in-data-[theme=night]:hidden']"
       />
+
       <img
         v-if="darkModeImage"
         :src="darkModeImage"
         :alt="name"
-        class="w-full hidden dark:block"
+        class="w-full hidden in-data-[theme=night]:block"
       />
     </figure>
     <!-- 8 empty divs needed for the 3D effect -->
@@ -31,7 +31,7 @@
 defineProps<{
   name: string;
   image: string;
-  darkModeImage?: string ;
+  darkModeImage?: string;
 }>();
 </script>
 
